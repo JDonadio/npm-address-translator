746de930ee57246ae461b0bd163f0179710fbfb6
JDonadio <tanojaja@gmail.com>
2017-09-27 16:50:58 -0300

    ref index exports

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
-var bitcore = require('bitcore-lib');
-var bitcoreCash = require('bitcore-lib-cash');
-var Bitcore = {};
+exports.Translator = function() {
+  var bitcore = require('bitcore-lib');
+  var bitcoreCash = require('bitcore-lib-cash');
+  var Bitcore = {};
 
-Bitcore = {
-  'btc': {
-    lib: bitcore,
-    translateTo: 'bch'
-  },
-  'bch': {
-    lib: bitcoreCash,
-    translateTo: 'btc'
-  }
-};
-
-function getAddressCoin(address) {
-  try {
-    new Bitcore['btc'].lib.Address(address);
-    return 'btc';
-  } catch (e) {
-    try {
-      new Bitcore['bch'].lib.Address(address);
-      return 'bch';
-    } catch (e) {
-      return null;
+  Bitcore = {
+    'btc': {
+      lib: bitcore,
+      translateTo: 'bch'
+    },
+    'bch': {
+      lib: bitcoreCash,
+      translateTo: 'btc'
     }
-  }
-};
+  };
 
-function translateAddress(address) {
-  var origCoin = getAddressCoin(address);
-  if (!origCoin) return;
+  function getAddressCoin(address) {
+    try {
+      new Bitcore['btc'].lib.Address(address);
+      return 'btc';
+    } catch (e) {
+      try {
+        new Bitcore['bch'].lib.Address(address);
+        return 'bch';
+      } catch (e) {
+        return null;
+      }
+    }
+  };
 
-  var origAddress = new Bitcore[origCoin].lib.Address(address);
-  var origObj = origAddress.toObject();
+  function translateAddress(address) {
+    var origCoin = getAddressCoin(address);
+    if (!origCoin) return;
 
-  var resultCoin = Bitcore[origCoin].translateTo;
-  var resultAddress = Bitcore[resultCoin].lib.Address.fromObject(origObj);
-  return {
-    origCoin: origCoin.toUpperCase(),
-    origAddress: address,
-    resultCoin: resultCoin.toUpperCase(),
-    resultAddress: resultAddress.toString()
+    var origAddress = new Bitcore[origCoin].lib.Address(address);
+    var origObj = origAddress.toObject();
+
+    var resultCoin = Bitcore[origCoin].translateTo;
+    var resultAddress = Bitcore[resultCoin].lib.Address.fromObject(origObj);
+    return {
+      origCoin: origCoin.toUpperCase(),
+      origAddress: address,
+      resultCoin: resultCoin.toUpperCase(),
+      resultAddress: resultAddress.toString()
+    };
   };
 };
-
-module.exports = Translator;
